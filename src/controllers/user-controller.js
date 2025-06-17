3400540fb5355fe45e1339bf5af9afb74bc869a9
Gaurav singh lodhi <lodhigaurav23@gmail.com>
2025-06-17 23:07:07 +0530

    Added new user signup api

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,31 @@
+const { StatusCodes } = require("http-status-codes");
+const { UserService } = require("../services");
+const { ErrorResponse, SuccessResponse } = require("../utils/common");
+
+
+/**
+ * 
+ * POST : /signup
+ * req-body : {email : 'a@borro.com', password: '1233'}
+ * 
+ *  
+ */
+
+async function signup(req, res) {
+  try {
+    // console.log("inside controller routes",req.body);
+    const user = await UserService.create({
+      email: req.body.email,
+      password : req.body.password
+    });
+    SuccessResponse.data = user;
+    return res.status(StatusCodes.CREATED).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+module.exports = {
+  signup
+}
