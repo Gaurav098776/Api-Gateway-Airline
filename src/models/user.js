86a803afb86a1fd6daed08008de4e74265cff939
Gaurav singh lodhi <lodhigaurav23@gmail.com>
2025-06-18 13:43:45 +0530

    Added encryption in signup

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const bcrypt = require("bcrypt");
+const { SALT_ROUNDS } = require("../config/server-config");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -13,25 +13,35 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    email: {
-      type :  DataTypes.STRING,
-      allowNull: false,
-      unique : true,
-      validate :{
-        isEmail: true
-      }
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 50],
+        },
+      },
     },
-    password: {
-      type : DataTypes.STRING,
-      allowNull: false,
-       validate: {
-          len: [3, 50]
-        }
+    {
+      sequelize,
+      modelName: "User",
     }
-  }, {
-    sequelize,
-    modelName: 'User',
+  );
+
+  User.beforeCreate(function encrypt(user) {
+    console.log("this user obj before encryption", user);
+    const encryptPassword = bcrypt.hashSync(user.password, +SALT_ROUNDS);
+    user.password = encryptPassword;
+    console.log("this user obj before encryption", user);
   });
   return User;
 };
